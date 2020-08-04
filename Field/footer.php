21be48af18c01e65400c54c74f3c2677696096c2
thetuningspoon <mike@thetuningspoon.com>
2020-08-04 16:04:51 -0400

    Allow html in error and message text

--- Field/footer.php
+++ Field/footer.php
@@ -2,7 +2,7 @@
 		<div class="field-notes"><?= $notes ?></div>
 	<?php endif ?>
 
-	<div class="field-error" <?= !$error ? 'style="display:none"' : '' ?>><i class="fa fa-caret-up"></i> <span class="field-errorTxt"><?= $sanitizer->entities1($error) ?></span></div>
-    <div class="field-message" <?= !$error ? 'style="display:none"' : '' ?>><i class="fa fa-caret-up"></i> <span class="field-messageTxt"><?= $sanitizer->entities1($message) ?></span></div>
+	<div class="field-error" <?= !$error ? 'style="display:none"' : '' ?>><i class="fa fa-caret-up"></i> <span class="field-errorTxt"><?= $error ?></span></div>
+    <div class="field-message" <?= !$error ? 'style="display:none"' : '' ?>><i class="fa fa-caret-up"></i> <span class="field-messageTxt"><?= $message ?></span></div>
 
 </div>
