126382d97aaf3c12fe64da70a61ab784b9d70876
thetuningspoon <mike@thetuningspoon.com>
2020-08-06 12:41:34 -0400

    Fixes issue with message icon appearing when no message exists

--- Field/footer.php
+++ Field/footer.php
@@ -3,6 +3,6 @@
 	<?php endif ?>
 
 	<div class="field-error" <?= !$error ? 'style="display:none"' : '' ?>><i class="fa fa-caret-up"></i> <span class="field-errorTxt"><?= $error ?></span></div>
-    <div class="field-message" <?= !$error ? 'style="display:none"' : '' ?>><i class="fa fa-caret-up"></i> <span class="field-messageTxt"><?= $message ?></span></div>
+    <div class="field-message" <?= !$message ? 'style="display:none"' : '' ?>><i class="fa fa-caret-up"></i> <span class="field-messageTxt"><?= $message ?></span></div>
 
 </div>
