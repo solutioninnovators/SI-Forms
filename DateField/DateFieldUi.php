32d5161ec8f5b20839ca984a6d1a285e5842747c
thetuningspoon <mike@thetuningspoon.com>
2020-08-18 15:28:21 -0400

    Fixes mismatched default date formats in DateFieldUi.php

--- DateField/DateFieldUi.php
+++ DateField/DateFieldUi.php
@@ -2,8 +2,8 @@
 class DateFieldUi extends FieldUi {
 
 	public $cssClass = 'dateField';
-	public $jsDateFormat = 'Y-m-d';
-	public $phpDateFormat = 'YYYY-MM-DD';
+	public $jsDateFormat = 'YYYY-MM-DD';
+	public $phpDateFormat = 'Y-m-d';
 
 	public function setup() {
 		$this->headScripts[] = $this->config->urls->templates . "library/moment.min.js"; // Including moment.js enables additional date format options in pikaday
