fc72f5e92899ae1cb86c309ec84be54ab10cad7f
thetuningspoon <mike@thetuningspoon.com>
2020-10-06 11:00:01 -0400

    Adds searchSelectorCallback and resultsLabelCallback options to AutocompleteField

--- AutocompleteField/AutocompleteFieldUi.php
+++ AutocompleteField/AutocompleteFieldUi.php
@@ -13,10 +13,12 @@ class AutocompleteFieldUi extends FieldUi {
 	public $searchFields = 'title'; // Pipe separated list of field names to search
 	public $searchById = true; // Show matching result in the autocomplete when a valid page ID is entered directly
 	public $__searchSelector; // A selector string to narrow down the pages that will be searched
+	public $searchSelectorCallback; // For more complex scenarios. This is a custom callback function that is passed the $query and returns a selector string. Overrides $searchSelector and $searchById
 	public $__validateSelector; // Optional selector string to validate the user's input. If not specified, the $searchSelector will be used. Use this when you want to allow certain values but don't want them to show in the autocomplete search results.
 	public $resultsLabel = 'title'; // Field name to use for the result label
-	public $resultsLabelString; // Use for complex labels with multiple data fields. Specify one or more field names surrounded by curly {brackets} along with any additional characters, spacing or punctuation.
-	public $allowStringValue = false; // Allows the field to accept an arbitrary string value as an alternative to a page ID
+	public $resultsLabelString; // Use for labels that require multiple data fields. Specify one or more field names surrounded by curly {brackets} along with any additional characters, spacing or punctuation.
+	public $resultsLabelCallback; // For more complex label requirements, provide a custom callback function. Your function will be passed a $page representing the result and should return a string representing the resulting label.
+    public $allowStringValue = false; // Allows the field to accept an arbitrary string value as an alternative to a page ID
 	public $resultLimit = 10;
 	public $searchOperator = '%='; // ProcessWire search operator to use to match results
 	public $groupBy; // Name of ProcessWire field to group the suggestions by (if grouping the suggestions is desired)
@@ -46,8 +48,13 @@ class AutocompleteFieldUi extends FieldUi {
 		$query = str_replace('"', '', $query); // Remove any existing double quotes from the input
 		$query = "\"$query\""; // Wrap the string in double quotes to prevent injection attacks
 
-		$selector = "{$this->searchSelector}, limit={$this->resultLimit}, q=({$this->searchFields}{$this->searchOperator}{$query})";
-		if($this->searchById) $selector .= ",q=(id={$query})";
+		if($this->searchSelectorCallback) {
+			$selector = call_user_func_array($this->searchSelectorCallback, [$query]);
+		}
+		else {
+			$selector = "{$this->searchSelector}, limit={$this->resultLimit}, q=({$this->searchFields}{$this->searchOperator}{$query})";
+			if($this->searchById) $selector .= ",q=(id={$query})";
+		}
 
 		return $selector;
 	}
@@ -76,11 +83,14 @@ class AutocompleteFieldUi extends FieldUi {
 	}
 
 	protected function createLabel(Page $match) {
-		if(!$this->resultsLabelString) {
-			$label = $match->{$this->resultsLabel};
+		if($this->resultsLabelCallback) {
+			$label = call_user_func_array($this->resultsLabelCallback, [$match]);
+		}
+		elseif($this->resultsLabelString) {
+			$label = $match->getMarkup($this->resultsLabelString);
 		}
 		else {
-			$label = $match->getMarkup($this->resultsLabelString);
+			$label = $match->{$this->resultsLabel};
 		}
 
 		return $this->sanitizer->unentities($label); // We're encoding in the view. jQuery Autocomplete also does its own encoding, so this avoids double encoding.
